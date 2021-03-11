f0685709bd7df58ea3e9d1b5977315c0787e5006
felipe.siqueira <felipesiqgodoy@gmail.com>
2021-03-11 15:09:18 -0300

    Delete Event

--- src/main/java/com/pooii/ac1/controllers/EventController.java
+++ src/main/java/com/pooii/ac1/controllers/EventController.java
@@ -8,6 +8,7 @@ import com.pooii.ac1.services.EventService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,12 @@ public class EventController {
         Event e = service.insert(event);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(e.getId()).toUri();
         return ResponseEntity.created(uri).body(e);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
--- src/main/java/com/pooii/ac1/services/EventService.java
+++ src/main/java/com/pooii/ac1/services/EventService.java
@@ -7,6 +7,7 @@ import com.pooii.ac1.entities.Event;
 import com.pooii.ac1.repositories.EventRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -35,6 +36,15 @@ public class EventService {
         event = repository.save(e);
 
         return event;
+    }
+
+    public void delete(Long id) {
+        try{
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found");
+        }
     }
 
     
